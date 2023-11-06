04d3379e6fe5877dba8429f07829987633d9cfc5
unknown <erichkrause2012@tut.by>
2023-11-06 11:07:10 +0300

    fix deloy

--- src/pages/FullPost.jsx
+++ src/pages/FullPost.jsx
@@ -28,6 +28,10 @@ export const FullPost = () => {
     return <Post isLoading={isLoading} isFullPost />
   }
 
+  if(!data) {
+    data.tags = ["Какой-то тэг"]
+  }
+
   return (
     <>
       <Post
